4234313c28d014193e77c12535b868acd431503b
VitamiiN <47637877+VitamiiN@users.noreply.github.com>
2021-02-22 20:48:40 -0500

    Add files via upload

--- pyps.py
+++ pyps.py
@@ -4,20 +4,35 @@ import subprocess
 from pathlib import Path
 import sys
 
+def printUsage(suppliedDupArgs=False):
+    if suppliedDupArgs == False:
+        print("Usage: pyps.py arg1 arg2")
+    else:
+        print("Error: supplied duplicate args.")
+        print("Usage: pyps.py arg1 arg2")
+
+    sys.exit()
 
 def main():
     p = Path("/proc/")
 
     fullFormat = False
+    allProc = False
+
     if "-h" in sys.argv or "--help" in sys.argv:
-        print("Usage: pyps.py arg1 arg2")
-        sys.exit()
+        printUsage()
 
     for param in sys.argv:
         if param == "-f":
             if fullFormat == False:
                 print("UID\t\tPID\tPPID\tCMD")
-            fullFormat = True
+                fullFormat = True
+            else:
+                printUsage(suppliedDupArgs=True)
+        elif param == "-A":
+            if allProc == True:
+                printUsage(suppliedDupArgs=True)
+            allProc = True
 
     if fullFormat == False:
         print("PID\tCMD")
@@ -34,6 +49,9 @@ def main():
                 fullCmdName = open(str(child) + "/cmdline", "r").read().replace("\n", "").replace("\0"," ")
                 cmdName = open(str(child) + "/comm", "r").read().replace("\n", "")
 
+                if fullCmdName == "":
+                    fullCmdName = "[" + cmdName + "]"
+
                 original_tty = open("/proc/" + str(os.getpid()) + "/stat").read().split(" ")[7]
                 tty = open(str(child) + "/stat", "r").read().split(" ")[7]
 
@@ -45,13 +63,17 @@ def main():
                             username = line.split(":")[0]
                 if len(username) > 7:
                     username = username[:7] + "+"
-
+            if allProc == False:
                 if int(self_euid) == int(uid) and tty != "0" and tty != "-1" and tty == original_tty:
                     if fullFormat == False:
                         print(pid + "\t" + cmdName)
                     else:
                         print(username + "\t" + pid + "\t" + ppid + "\t" + fullCmdName)
-
+            else:
+                if fullFormat == False:
+                    print(pid + "\t" + cmdName)
+                else:
+                    print(username + "\t" + pid + "\t" + ppid + "\t" + fullCmdName)
 
 
 if __name__ == '__main__':
